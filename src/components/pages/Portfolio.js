64296f54895d9c0131f2c70f2ec24edaccaeec9e
harrj396 <ahletmethink@gmail.com>
2022-12-12 03:33:47 -0500

    Added cards to portfolio and added placeholder image to project 3

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,12 +1,51 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Button from 'react-bootstrap/Button';
 
-export default function Portfolio() {
+function GroupExample() {
   return (
-    <div>
-      <h1></h1>
-      <p>
-       
-      </p>
-    </div>
+    <CardGroup>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+        <Button variant="secondary">Go somewhere</Button>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.{' '}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+        <Button variant="secondary">Go somewhere</Button>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://via.placeholder.com/800x600" />
+        <Card.Body>
+          <Card.Title>Project 3</Card.Title>
+          <Card.Text>
+            This is a placeholder for my third project currently in development.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+        <Button variant="secondary">Go somewhere</Button>
+        </Card.Footer>
+      </Card>
+    </CardGroup>
   );
 }
+
+export default GroupExample;
